1d3927eebedd16986ea2f41a2f56933a5257898c
Jacobo Tarragón Cros <jacobo.tarragon@gmail.com>
2024-05-12 11:52:39 -0400

    add custom amounts

--- kiosko/src/Catalog.js
+++ kiosko/src/Catalog.js
@@ -5,7 +5,7 @@ function Item(section, item) {
     const addItem = useStore((store) => store.addItem);
 
     return (
-        <div className={"item " + item.style} onClick={addItem(section.title, item)}>
+        <div className={"item " + item.style} onClick={addItem(item)}>
             <div className="item-name"><p>{item.name}</p></div>
             <div className="item-price"><p>{item.price}€</p></div>
         </div>
@@ -17,7 +17,7 @@ function Section(section) {
         <div>
             <h1>{section.title}</h1>
             <div className="section-items">
-                {section.items.map(i => <div key={i.id || i.name}>{Item(section, i)}</div>)}
+                {section.items.map(i => <div key={i.code}>{Item(section, i)}</div>)}
             </div>
         </div>
     );
@@ -25,7 +25,7 @@ function Section(section) {
 
 function Catalog({data}) {
     return (<>
-        {data.sections.map(s => <section key={s.id || s.name}>{Section(s)}</section>)}
+        {data.sections.map(s => <section key={s.code}>{Section(s)}</section>)}
     </>);
 }
 
--- kiosko/src/Numpad.js
+++ kiosko/src/Numpad.js
@@ -1,25 +1,49 @@
 
+import {useState} from 'react';
+import { useStore } from './store.js';
+
+
 function Numpad() {
+
+    const [num, setNum] = useState("");
+    const [isDecimal, setIsDecimal] = useState(false);
+    const addAmount = useStore((state) => state.addAmount);
+
+    function addNum(num, n) {
+        setNum(num + n);
+    }
+
+    function addCustomAmount(amount) {
+        const price = Number(num);
+        setNum("");
+        return addAmount(price)();
+    }
+
     return (
         <div id="numpad">
-            <h1 class="display">
-                NUM
+            <h1 className="display">
+                {num || 0}
             </h1>
 
-            <div class="num num7">7</div>
-            <div class="num num8">8</div>
-            <div class="num num9">9</div>
-            <div class="num num4">4</div>
-            <div class="num num5">5</div>
-            <div class="num num6">6</div>
-            <div class="num num1">1</div>
-            <div class="num num2">2</div>
-            <div class="num num3">3</div>
-            <div class="num num0">0</div>
-            <div class="numD">.</div>
-            <div class="clear">C</div>
+            <div onClick={() => addNum(num, "7")} className="num num7">7</div>
+            <div onClick={() => addNum(num, "8")} className="num num8">8</div>
+            <div onClick={() => addNum(num, "9")} className="num num9">9</div>
+            <div onClick={() => addNum(num, "4")} className="num num4">4</div>
+            <div onClick={() => addNum(num, "5")} className="num num5">5</div>
+            <div onClick={() => addNum(num, "6")} className="num num6">6</div>
+            <div onClick={() => addNum(num, "1")} className="num num1">1</div>
+            <div onClick={() => addNum(num, "2")} className="num num2">2</div>
+            <div onClick={() => addNum(num, "3")} className="num num3">3</div>
+            <div onClick={() => addNum(num, "0")} className="num num0">0</div>
 
-            <div class="amount">ADD</div>
+            <div onClick={() => setIsDecimal(!isDecimal)}
+                 className={"numD " + (isDecimal ? 'active' : '')} >
+                .
+            </div>
+
+            <div onClick={() => setNum("")} className="clear">C</div>
+
+            <div onClick={() => addCustomAmount()} className="amount">ADD</div>
         </div>
     )
 }
